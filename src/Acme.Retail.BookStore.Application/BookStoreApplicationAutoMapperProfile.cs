40eeb52a6605d059a9cbf9018187893c64dee34c
Ramy Youssef <ramy.youssef@atos.net>
2022-05-24 08:44:56 +0200

    Part 1: Creating the server side

--- src/Acme.Retail.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
+++ src/Acme.Retail.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using Acme.Retail.BookStore.Books;
+using AutoMapper;
 
 namespace Acme.Retail.BookStore;
 
@@ -9,5 +10,7 @@ public class BookStoreApplicationAutoMapperProfile : Profile
         /* You can configure your AutoMapper mapping configuration here.
          * Alternatively, you can split your mapping configurations
          * into multiple profile classes for a better organization. */
+        CreateMap<Book, BookDto>();
+        CreateMap<CreateUpdateBookDto, Book>();
     }
 }
