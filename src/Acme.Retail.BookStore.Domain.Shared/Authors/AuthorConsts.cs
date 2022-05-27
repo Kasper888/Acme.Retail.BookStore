cb692aaa57456e56c0d9c68f1c7b6275cb818065
Ramy Youssef <ramy.youssef@atos.net>
2022-05-28 00:34:29 +0200

    Part 6: Authors: Domain Layer

---
+++ src/Acme.Retail.BookStore.Domain.Shared/Authors/AuthorConsts.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Acme.Retail.BookStore.Authors
+{
+    public static class AuthorConsts
+    {
+        public const int MaxNameLength = 64;
+    }
+}
